4419025ff9129338e9730c97dc7dd7c81d1f56e0
anreonyr <anreonyr@gmail.com>
2026-07-30 14:36:24 +0800

    drivers: discovery → tree

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,12 +1,12 @@
 pub mod clint;
 pub mod device;
-pub mod discovery;
+pub mod tree;
 pub mod plic;
 pub mod uart;
 
 // 只 re-export 访问器函数，具体类型名不对外暴露
 pub use clint::CLINT;
-pub use discovery::{discover, for_each};
+pub use tree::{discover, for_each};
 pub use plic::PLIC;
 pub use uart::UART;
 
